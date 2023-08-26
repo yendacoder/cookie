c2378fc58c5bd5bfc97b608702b8a59c4d62532a
Yendacoder <t@k7a.me>
2023-08-26 14:56:46 +0200

    [feature] showing communities on the profile screen

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -1,9 +1,14 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cookie/api/model/community.dart';
+import 'package:cookie/api/model/enums.dart';
 import 'package:cookie/common/controller/initial_controller.dart';
 import 'package:cookie/common/ui/notifications.dart';
 import 'package:cookie/common/ui/widgets/common/icon_text.dart';
+import 'package:cookie/common/ui/widgets/common/tappable_item.dart';
+import 'package:cookie/common/ui/widgets/community_icon.dart';
 import 'package:cookie/common/ui/widgets/user_image.dart';
 import 'package:cookie/common/util/context_util.dart';
+import 'package:cookie/router/router.gr.dart';
 import 'package:cookie/settings/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
@@ -65,10 +70,126 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
+  Widget _buildCommunity(BuildContext context, Community community) {
+    return TappableItem(
+      child: ListTile(
+        leading: CommunityIcon(
+          image: community.proPic,
+        ),
+        title: Text(community.name),
+      ),
+      onTap: () => context.router.push(FeedRoute(
+          feedType: FeedType.community.name, communityId: community.id)),
+    );
+  }
+
+  Widget _buildHeader(
+      BuildContext context, InitialController initialController) {
+    final theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (!initialController.isLoggedIn) ...[
+          PlatformTextField(
+            controller: _usernameController,
+            hintText: context.l.authUsernameHint,
+          ),
+          const SizedBox(height: kSecondaryPadding),
+          PlatformTextField(
+            controller: _passwordController,
+            obscureText: true,
+            hintText: context.l.authPasswordHint,
+          ),
+          const SizedBox(height: kSecondaryPadding),
+          PlatformElevatedButton(
+              child: Text(context.l.authLogin),
+              onPressed: () => _login(initialController)),
+          const SizedBox(height: kSecondaryPadding),
+          PlatformTextButton(
+              child: Text(context.l.authRegister),
+              onPressed: () => launchUrlString('https://discuit.net/',
+                  mode: LaunchMode.externalApplication)),
+        ] else ...[
+          const SizedBox(
+            height: kSecondaryPadding,
+          ),
+          Center(
+            child: UserImage(
+              username: initialController.initial!.user!.username,
+              size: 80,
+            ),
+          ),
+          Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: kSecondaryPadding),
+              child: Text(
+                initialController.initial!.user!.username,
+                style: theme.textTheme.headlineMedium,
+              )),
+          _buildProfileItem(context, Icons.score,
+              context.l.profilePoints(initialController.initial!.user!.points)),
+          _buildProfileItem(context, Icons.message,
+              context.l.profilePosts(initialController.initial!.user!.noPosts)),
+          _buildProfileItem(
+              context,
+              Icons.comment,
+              context.l.profileComments(
+                  initialController.initial!.user!.noComments)),
+        ],
+        if ((initialController.initial?.communities.length ?? 0) > 0)
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: kPrimaryPadding),
+            child: Text(
+              context.l.profileCommunities,
+              style: theme.textTheme.titleMedium,
+            ),
+          )
+      ],
+    );
+  }
+
+  Widget _buildFooter(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Divider(),
+        _buildMenuItem(
+            context,
+            context.l.profileAbout,
+            Icons.info,
+            () => launchUrlString(
+                'https://github.com/yendacoder/cookie/blob/main/README.md',
+                mode: LaunchMode.externalApplication)),
+        _buildMenuItem(
+            context, context.l.profileOpenSourceLicenses, Icons.text_snippet,
+            () {
+          showLicensePage(context: context);
+        }),
+        FutureBuilder<PackageInfo>(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done &&
+                  snapshot.hasData &&
+                  snapshot.data?.version != null) {
+                return Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, kPrimaryPadding,
+                        kSecondaryPadding, kSecondaryPadding),
+                    child: Text(
+                        context.l.profileVersion(snapshot.data!.version),
+                        style: theme.textTheme.bodySmall));
+              }
+              return const SizedBox();
+            })
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final initialController = Provider.of<InitialController>(context);
-    final theme = Theme.of(context);
+    final communitiesCount =
+        (initialController.initial?.communities.length ?? 0);
     return PlatformScaffold(
       appBar: PlatformAppBar(
         title: Text(
@@ -81,94 +202,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 onPressed: () => _logout(initialController))
         ],
       ),
-      body: Padding(
+      body: ListView.builder(
+        itemCount: communitiesCount + 2,
         padding: const EdgeInsets.all(kPrimaryPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            if (!initialController.isLoggedIn) ...[
-              PlatformTextField(
-                controller: _usernameController,
-                hintText: context.l.authUsernameHint,
-              ),
-              const SizedBox(height: kSecondaryPadding),
-              PlatformTextField(
-                controller: _passwordController,
-                obscureText: true,
-                hintText: context.l.authPasswordHint,
-              ),
-              const SizedBox(height: kSecondaryPadding),
-              PlatformElevatedButton(
-                  child: Text(context.l.authLogin),
-                  onPressed: () => _login(initialController)),
-              const SizedBox(height: kSecondaryPadding),
-              PlatformTextButton(
-                  child: Text(context.l.authRegister),
-                  onPressed: () => launchUrlString('https://discuit.net/',
-                      mode: LaunchMode.externalApplication)),
-            ] else ...[
-              const SizedBox(
-                height: kSecondaryPadding,
-              ),
-              Center(
-                child: UserImage(
-                  username: initialController.initial!.user!.username,
-                  size: 80,
-                ),
-              ),
-              Container(
-                  alignment: Alignment.center,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: kSecondaryPadding),
-                  child: Text(
-                    initialController.initial!.user!.username,
-                    style: theme.textTheme.headlineMedium,
-                  )),
-              _buildProfileItem(
-                  context,
-                  Icons.score,
-                  context.l
-                      .profilePoints(initialController.initial!.user!.points)),
-              _buildProfileItem(
-                  context,
-                  Icons.message,
-                  context.l
-                      .profilePosts(initialController.initial!.user!.noPosts)),
-              _buildProfileItem(
-                  context,
-                  Icons.comment,
-                  context.l.profileComments(
-                      initialController.initial!.user!.noComments)),
-            ],
-            const Spacer(),
-            _buildMenuItem(
-                context,
-                context.l.profileAbout,
-                Icons.info,
-                () => launchUrlString(
-                    'https://github.com/yendacoder/cookie/blob/main/README.md',
-                    mode: LaunchMode.externalApplication)),
-            _buildMenuItem(context, context.l.profileOpenSourceLicenses,
-                Icons.text_snippet, () {
-              showLicensePage(context: context);
-            }),
-            FutureBuilder<PackageInfo>(
-                future: PackageInfo.fromPlatform(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done &&
-                      snapshot.hasData &&
-                      snapshot.data?.version != null) {
-                    return Padding(
-                        padding: const EdgeInsets.fromLTRB(0.0, kPrimaryPadding,
-                            kSecondaryPadding, kSecondaryPadding),
-                        child: Text(
-                            context.l.profileVersion(snapshot.data!.version),
-                            style: theme.textTheme.bodySmall));
-                  }
-                  return const SizedBox();
-                })
-          ],
-        ),
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return _buildHeader(context, initialController);
+          } else if (index > communitiesCount) {
+            return _buildFooter(context);
+          } else {
+            return _buildCommunity(
+                context, initialController.initial!.communities[index - 1]);
+          }
+        },
       ),
     );
   }
