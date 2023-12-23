00cd223d4a821d97928e2ccf9ca1c8f6b483d6b0
Yendacoder <it@yendoplan.com>
2023-12-23 17:07:13 +0100

    [fix] fixed compatibility issue caused by API changes

--- lib/api/model/image_copy.dart
+++ lib/api/model/image_copy.dart
@@ -5,18 +5,14 @@ part 'image_copy.g.dart';
 @JsonSerializable()
 class ImageCopy {
   const ImageCopy(
-      this.copyId,
       this.width,
       this.height,
-      this.size,
       this.url,
       this.objectFit,
       );
 
-  final String copyId;
   final int width;
   final int height;
-  final int size;
   final String url;
   final String objectFit;
 
--- lib/api/model/image_copy.g.dart
+++ lib/api/model/image_copy.g.dart
@@ -7,19 +7,15 @@ part of 'image_copy.dart';
 // **************************************************************************
 
 ImageCopy _$ImageCopyFromJson(Map<String, dynamic> json) => ImageCopy(
-      json['copyId'] as String,
       json['width'] as int,
       json['height'] as int,
-      json['size'] as int,
       json['url'] as String,
       json['objectFit'] as String,
     );
 
 Map<String, dynamic> _$ImageCopyToJson(ImageCopy instance) => <String, dynamic>{
-      'copyId': instance.copyId,
       'width': instance.width,
       'height': instance.height,
-      'size': instance.size,
       'url': instance.url,
       'objectFit': instance.objectFit,
     };
