e3b7c8449bac9e3bdf869028c7a53965b08b9207
Yendacoder <it@yendoplan.com>
2024-07-20 16:08:59 +0200

    [fix] fixed image posting

--- lib/api/model/uploaded_image.g.dart
+++ lib/api/model/uploaded_image.g.dart
@@ -9,31 +9,17 @@ part of 'uploaded_image.dart';
 UploadedImage _$UploadedImageFromJson(Map<String, dynamic> json) =>
     UploadedImage(
       json['id'] as String,
-      json['type'] as String,
       json['width'] as int,
-      json['maxWidth'] as int,
       json['height'] as int,
-      json['maxHeight'] as int,
       json['size'] as int,
-      Color.fromJson(json['averageColor'] as Map<String, dynamic>),
       json['url'] as String,
-      (json['urls'] as List<dynamic>).map((e) => e as String).toList(),
-      DateTime.parse(json['createdAt'] as String),
-      json['deleted'] as bool,
     );
 
 Map<String, dynamic> _$UploadedImageToJson(UploadedImage instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'type': instance.type,
       'width': instance.width,
-      'maxWidth': instance.maxWidth,
       'height': instance.height,
-      'maxHeight': instance.maxHeight,
       'size': instance.size,
-      'averageColor': instance.averageColor,
       'url': instance.url,
-      'urls': instance.urls,
-      'createdAt': instance.createdAt.toIso8601String(),
-      'deleted': instance.deleted,
     };
