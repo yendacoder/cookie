8fb79a9702f0e1af9cb36941c86a76c0093f7d46
Yendacoder <it@yendoplan.com>
2024-07-20 16:08:59 +0200

    [fix] fixed image posting

--- lib/api/model/uploaded_image.dart
+++ lib/api/model/uploaded_image.dart
@@ -1,41 +1,22 @@
 import 'package:json_annotation/json_annotation.dart';
 
-import 'color.dart';
-
 part 'uploaded_image.g.dart';
 
 @JsonSerializable()
 class UploadedImage {
   UploadedImage(
     this.id,
-    this.type,
     this.width,
-    this.maxWidth,
     this.height,
-    this.maxHeight,
     this.size,
-    this.averageColor,
     this.url,
-    this.urls,
-    // this.copies,
-    this.createdAt,
-    this.deleted,
   );
 
   final String id;
-  final String type;
   final int width;
-  final int maxWidth;
   final int height;
-  final int maxHeight;
   final int size;
-  final Color averageColor;
   final String url;
-  final List<String> urls;
-  // Image copy object is different, let's skip it for now
-  // final List<ImageCopy> copies;
-  final DateTime createdAt;
-  final bool deleted;
 
   @JsonKey(includeFromJson: false, includeToJson: false)
   double? calculatedLinkImageRatio;
