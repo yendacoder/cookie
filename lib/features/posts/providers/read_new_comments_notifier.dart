a7dc625f8d911f6bf597fe9e9954dd442b61c518
Yendacoder <yendo@k7a.me>
2026-05-30 12:46:59 +0200

    version bump, lint fixes

--- lib/features/posts/providers/read_new_comments_notifier.dart
+++ lib/features/posts/providers/read_new_comments_notifier.dart
@@ -1,7 +1,4 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-import '../../../core/api/api_client.dart';
-
 part 'read_new_comments_notifier.g.dart';
 
 /// When a post detail screen loads successfully, it might want to
